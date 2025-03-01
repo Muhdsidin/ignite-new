f021942dab571545371282cffa65ddfa706afc3d
Logic-leap <logicleap00@gmail.com>
2025-03-02 00:23:46 +0530

    edited navbar

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,7 +2,7 @@ import { HeroGeometric } from "@/components/ui/shape-landing-hero"
 
 
 function Hero() {
-    return <HeroGeometric badge="Igniteway"
+    return <HeroGeometric  badge="Igniteway"
             title1 = "Invest Your Idea"
             title2 = "Build Your Dream" />
 }
--- src/components/ui/footer-section.jsx
+++ src/components/ui/footer-section.jsx
@@ -50,7 +50,7 @@ function Footerdemo() {
 
   return (
     (<div
-      className="relative border-t bg-black text-foreground transition-colors duration-300">
+      className="relative border-t bg-black text-foreground transition-colors duration-300" id="Contact">
       <div className="container mx-auto px-4 py-12 md:px-6 lg:px-8">
         <div className="grid gap-12 md:grid-cols-2 lg:grid-cols-4">
           <div className="relative">
