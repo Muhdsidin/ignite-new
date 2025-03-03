898b13f68138197da96441daf31818bcee0cf270
Logic-leap <logicleap00@gmail.com>
2025-03-03 06:36:37 +0530

    adding

--- src/components/scroller/Scroll.jsx
+++ src/components/scroller/Scroll.jsx
@@ -7,10 +7,10 @@ const images = [
   "https://p0s6j4vdsr.ufs.sh/f/kIhPyf5uPqQMzJQ7xaNoZ3G2rHXoPdKuFTjBv5QNVW4mREah",
   
   "https://p0s6j4vdsr.ufs.sh/f/kIhPyf5uPqQMJeCxOsbsQmL4FIPjOkhuG8p6UKY3qb0n9V2v",
-  "https://p0s6j4vdsr.ufs.sh/f/kIhPyf5uPqQMZQEdgRrY7LJ9eqvB1ajVKuU0Wbk4HxwtfpQP",
-  "https://p0s6j4vdsr.ufs.sh/f/kIhPyf5uPqQMZQEdgRrY7LJ9eqvB1ajVKuU0Wbk4HxwtfpQP",
-  "https://p0s6j4vdsr.ufs.sh/f/kIhPyf5uPqQMZQEdgRrY7LJ9eqvB1ajVKuU0Wbk4HxwtfpQP",
-  "https://p0s6j4vdsr.ufs.sh/f/kIhPyf5uPqQMZQEdgRrY7LJ9eqvB1ajVKuU0Wbk4HxwtfpQP",
+  "https://p0s6j4vdsr.ufs.sh/f/kIhPyf5uPqQM89EIm91gqae68lmn9dtJW2FKbLczfiBCI3SZ"
+  // "https://p0s6j4vdsr.ufs.sh/f/kIhPyf5uPqQMZQEdgRrY7LJ9eqvB1ajVKuU0Wbk4HxwtfpQP",
+  // "https://p0s6j4vdsr.ufs.sh/f/kIhPyf5uPqQMZQEdgRrY7LJ9eqvB1ajVKuU0Wbk4HxwtfpQP",
+  // "https://p0s6j4vdsr.ufs.sh/f/kIhPyf5uPqQMZQEdgRrY7LJ9eqvB1ajVKuU0Wbk4HxwtfpQP",
 ];
 
 function Scroll() {
